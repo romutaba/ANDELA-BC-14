ccdbe3cfca00c437fe3997d2b1e3ee8ea1e1a254
RayRay <romutaba@users.noreply.github.com>
2017-01-17 10:04:28 +0300

    Adding code annotation

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,12 +1,17 @@
-prime_num_count = 0 #intitializing to Zero
+prime_num_count = 0 #intitializing count to Zero
+#prompt user for input
 x = int(input("Enter Number to check: "))
 def is_prime(x):
+    #ensure 1 and 0 are not treated as prime numbers by returning the boolean False
     if x in (0,1):
         return False
+    #checking for non-prime numbers between 2 and user input
     for y in range (2, x):
         if not (x % y):
             return False
+    #return True if prime 
     return True
+#printing prime numbers fouynd between range of zero and user input
 for i in range(0, x):
     if is_prime(i):
         prime_num_count += 1
