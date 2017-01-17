422fa0018f59842a7f0d874a2ab14f014463d7a5
RayRay <romutaba@users.noreply.github.com>
2017-01-17 10:42:40 +0300

    Add Messages to the Test

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -11,11 +11,11 @@ class prime_numbersTestCase(unittest.TestCase):
 
 	def test_is_three_prime(self):
 
-		self.assertTrue(is_prime(3))
+		self.assertTrue(is_prime(3), msg = "3 is a prime number")
 
 	def test_is_one_prime(self):
 
-		self.assertFalse(is_prime(1))	
+		self.assertFalse(is_prime(1), msg = "1 is NOT a prime number")	
 
 
 if __name__ =='__main__':
