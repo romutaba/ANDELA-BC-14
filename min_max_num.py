6280b9f17aa542f8fd2b3df21132eab980018f6e
unknown <romutaba@ymail.com>
2017-01-18 11:26:02 +0300

    code annotation

--- min_max_num.py
+++ min_max_num.py
@@ -1,14 +1,18 @@
 def find_max_min(num):
-
+	#create an empty list
 	min_max_list = []
+	#assign input to a list
 	num_list = num
+	#sort the list
 	num_list.sort()
+	#assign smallest number in list to variable min_num 
 	min_num = num_list[0]
+	#assign largest number in list to variable max_num 
 	max_num = num_list[-1]
-	
+	#checks if smallest and largest number are equal and appends to new list
 	if (min_num == max_num):
 		min_max_list.append(min_num)
-
+	#if the numbers arent equal append to min_max_list and sort list
 	else:		
 		min_max_list.append(min_num)
 		min_max_list.append(max_num)
